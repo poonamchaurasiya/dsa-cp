0c7b0381701f4bd1afeb37c38d2939c659c51e85
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-10 19:05:12 +0530

    array element deletion logic

---
+++ deletion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int deleteEle(int arr[],int n,int x)
+{
+    int i;
+    for( i=0;i<n;i++)
+    if(arr[i]==x)
+    break;
+    if(i==n)
+    return n;
+    for (int j=i;j<n-1;j++)
+    arr[j]= arr[j+1];
+    return (n-1);
+}
