9566932670a4e2fbf96e594448214f0234c02216
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-11 21:30:55 +0530

    using natural num sum using res

---
+++ array/LR an arr by D .cpp
@@ -0,0 +1,11 @@
+#include < iostream>
+using namespace std ;
+void leftRoatete (int arr[],int n)
+{
+    int temp[d];
+    for (int i=0;i<n;i++)
+    temp[i] =arr [i];
+    arr [i-d]= ar[i];
+    for (int i=0; i<d,i++)
+    arr[n-d+i]=temp[i];    
+}
