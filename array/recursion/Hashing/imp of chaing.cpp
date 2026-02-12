4566c42904c79f5a302dcd5ddbce1c28b7e5f998
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-12 18:58:51 +0530

    chaing logic

---
+++ array/recursion/Hashing/imp of chaing.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+{
+    int BUCKET ;
+    list < int < * table
+    :: :: ::
+    void insert (int key)
+    {
+        int i = key%BUCKET;
+        table [i].push -back (key);
+    }
+    void remove (int key)
+    {
+        int i = key% BUCKET:
+        table [i].remove (key);
+
+    }
+    :: :: ::
+
+    }
