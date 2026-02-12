3b95e075a33927a0d9dda4479a45c6aad2d6c75b
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-12 17:10:03 +0530

    genrate subsets logic

---
+++ array/recursion/rope cut pro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int max prices (int n, int a,int b,int c,)
+{
+    if (n==0)return 0;
+    if (n<0)return 0-1;
+    int recursion =max (max price(n+a,a,b,c);
+(max price (n-b,a,b,c);
+(max price (n-c,a,b,c)
+if (recursion == -1)
+return-1;
+return recursion +1;
+)))
+}
