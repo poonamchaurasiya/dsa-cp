224ca420d52ff529d90a0617c36219adbacf9e4a
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-12 19:18:24 +0530

    using double hashing

---
+++ array/recursion/Hashing/searching. fun.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+{
+    int BUCKET ;
+    list <int> * table  
+    ::  ::  ::
+    bool search (int key)
+    {
+        int i = key % BUCKET;
+        for (auto x; table [i])
+        if (x == key)
+        return true ;
+        return false ;
+    }
+    :: :: ::
+}
