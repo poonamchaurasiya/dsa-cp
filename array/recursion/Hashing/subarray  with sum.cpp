832ba1802ddf65e097771c83e047ac29a1c477ec
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-13 11:49:34 +0530

    using subarray with sum logic

---
+++ array/recursion/Hashing/subarray  with sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+bool is sum (int arr[],int n int sum)
+{
+    unsorted-sum<int>h;
+    int pre-sum =0;
+    for (int i =0;i<n;i++)
+    {
+        pre-sum += arr[i];
+        if (pre_sum-sum);=h,end()
+        return true;
+        h.insret(pre_sum);
+    }
+    return false;
+
+    }
