684be81b3357a5a01942b1112231398c876793c8
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-13 19:12:14 +0530

    using advance level subarray with zero sum logic

---
+++ array/recursion/Hashing/same as pair.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+bool is pair (int curr[],int n,int sum)
+{
+    usorted -ht<int>h;
+    for (int i=0;i<n;i++)
+    {
+        if(h.find(sum-arr[i])!=h.endl)
+    return true;
+elseh.insert(arr[i]);}
+return;
+}
