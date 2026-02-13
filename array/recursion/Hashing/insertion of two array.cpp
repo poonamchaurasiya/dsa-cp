7470b74992f31c9bf8947bdd44b6cc14226c1722
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-13 18:40:42 +0530

    using insetrion of two array

---
+++ array/recursion/Hashing/insertion of two array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int insertion (int a[],int int b[],int ,int n)
+{
+    int res =0;
+    for (int i =0;i<m;i++)
+    {
+        bool flag = false ;
+        for (int j =0;j<i-1;j++)
+        if (a[j]==a[i]){
+            flag =true;break;
+            if(flag== true){
+                continue;}
+                for(int j=0;j<n;j++)
+                if(a[i]==b[j]){
+                    res++;break;}
+                }
+                return res ;
+
+                }
+
+            }
+        
