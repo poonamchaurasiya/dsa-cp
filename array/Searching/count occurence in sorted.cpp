07e509d27484cf5e89471cbc2e6490c05f152346
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-14 18:40:10 +0530

    using count 1s in a sorted binary search logic

---
+++ array/Searching/count occurence in sorted.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+{
+    int first =first occ(arr,n,x);
+    if(first == -1)
+    return 0;
+    else 
+    return(last occ(arr ,n,x)-first _1);
+}
+
---
+++ array/Searching/recursion BS.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+int last occ(int arr[],int n,int x)
+{
+    int low =0; high =n-1;
+    while(low<=high)
+    {
+        int mid =(low+high)/2;
+        if(arr[mid]<x)
+        low= mid+1;
+        else if(arr[mid]>x
+        high=mid-1;
+    else{
+        if(mid!=n-1::arr [mid]!=arr[mid+1])
+        rteurn mid;
+        else
+        low=mid+1;
+    }) 
+    }
+}
