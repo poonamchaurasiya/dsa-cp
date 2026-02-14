703c1fe33bf2de94d6557e07db1df6bca81be0c4
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-14 17:34:35 +0530

    using the analyesis of binary search

---
+++ array/Searching/analysis of BS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int b search(arr,n,x)
+{
+    (low=0;high)
+    {
+        mid =(low+high/2);
+        if (arr[mid]==x)
+        return mid;
+    if (arr[mid]<x)
+else
+high=mid -1;
+    }
+return-1;
+
+    }
---
+++ array/Searching/index of first occ.cpp
@@ -0,0 +1,7 @@
+#include <iosream>
+{int first occurence (int arr[],int n,int x)
+for (int i=0;i<n;
+i++)
+if (arr[i]==x)
+returni;
+}
