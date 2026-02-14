76d186615e26ed8c0d1a0a59bae41976f2762a62
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-14 18:25:34 +0530

    using recursive binary search

---
+++ array/Searching/inx of last occ in sorted.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int last occ 9int arr[],int low,int high,int high,int x,int n)
+{
+    if (low>high)
+    rteurn-1;
+    int mid =(low+high)/2;
+    if (arr[mid]>x
+    return last occ (arr,mid+1,high,n);
+else {
+    if(mid ==n-1::arr[mid]!=arr[mid+1])
+    return mid;
+    else
+    return last occ(arr,mid+1,high,n);
+} )
+}
---
+++ array/Searching/same as  index of first.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+{
+    int first(int arr[],int n,int x)
+    {
+        int low =0,high=n-1;
+        while(low<=high)
+        {
+            int mid =(low+high)/2;
+            if (arr[mid]>x)
+            high =mid-1;
+            else if (arr[mid]<x
+            low = mid+1;
+        else if(mid ==0::arr[mid-1]!
+        =arr[mid])
+    returnmid;
+else
+high=mid-1;)
+        }
+    }
+    return-1;
+}
