3109385e954d49ef41067701fa4f86193bfec712
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-14 16:13:12 +0530

    using longest common span with same sum in binary logic

---
+++ array/recursion/Hashing/longest common span with same sum in bin.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int max common (int arr1[],int arr 2[],int n)
+{
+    int res =0;
+    for (int i =0;i<n;i++)
+{
+    int sum 1=0;j<n;j++
+    {
+        sum1+=arr1[j];
+        sum2+=arr2[j];
+        if(sum1==sum2)
+        res =max(res,j-i+1);}
+    }
+    return res;
+}
+    
