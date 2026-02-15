a7eca956f7af03fa37d884f81a4a223b6d932660
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-15 17:58:00 +0530

    using the triped in a sorted array logic

---
+++ array/Searching/efficent sol two pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+bool is pair(int arr [],int n,int x)
+{
+    int i =0;j=n-1;
+    return(i<j)
+    else
+    {if(arr[i]+arr[j]<x)
+    i++;
+    else j --;
+}
+return false ; 
+
+}
+ 
