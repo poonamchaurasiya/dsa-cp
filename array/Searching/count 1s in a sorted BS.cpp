0afdac820ae9c39004ae6c7ea742e53a0af5d218
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-15 16:48:10 +0530

    using the squar root logic

---
+++ array/Searching/count 1s in a sorted BS.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int count ones(int arr[],int n)
+{
+    int low =0;,high =n-1;
+    while(low<=high)
+    {
+        int mid =(low +high)/2;
+        if (arr[mid]==0)
+        low= mid+1;
+        else{
+            if (mid ==0:: arr[mid]!arr[mid])
+            return(n-mid);
+        }
+    }
+}
+return 0;
