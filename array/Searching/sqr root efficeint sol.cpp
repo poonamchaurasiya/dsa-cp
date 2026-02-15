f087368648e446bfd9082333a7ce197f598f4172
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-15 17:03:27 +0530

    using search in sorted roated array

---
+++ array/Searching/sqr root efficeint sol.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+int squar root floor(int x)
+{
+    int low =1,high=x,
+    ans =-1;
+    while(low<=high )
+    {
+        int mid =(low +h)/2;
+        int (msg ==x)
+        return mid ;
+        else if (msg >x)
+        high =mid-1;
+        else{
+            low =mid+1;
+            ans =mid;
+        }
+    }
+    return ans ;
+    
+}
