a0ef70e5fb8f78bf56e5f9c7643b12a2c6be3849
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-15 17:40:01 +0530

    using the effeicent sol of peak element

---
+++ array/Searching/find a peak ele.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int get peak(int arr[],int )
+{
+if (n==1)return arr[0];
+if (arr[0]>=arr[n-2]return arr[n-1];
+for (int i =1;;i<n-1;i++)
+if if(arr[i]>arr[i-1])
+arr [i]<arr[i+1])
+return array[i];
+}
