df2977e01517f04aa281d2d4c61e14d859ad6228
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-15 17:47:48 +0530

    using two pointer oppr logic

---
+++ array/Searching/peak ele.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int get a peak(int arr [],int n)
+{
+    int low =0,high=n-1;
+    while(low<=high)
+    {
+        int mid=(low+high)/2;
+        if((mid ==0::arr[mid]<=arr[mid]))&&
+        (mid ==n-1::arr[mid+1]<=arr[mid])
+        return mid;
+        if (mid >0 && arr[mid-1]>arr =arr[mid])
+        high=mid-1;
+        else
+        low=mid+1;
+    }
+    
+}
