6fd11503e3858738297398820013959794cb2e28
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-16 17:52:35 +0530

    using median of two sorted array logic

---
+++ array/Searching/Triped in sorted arr.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+bool is triplet(int arr[],int n, int x)
+{
+    for (int i =o;i<n-2;i++)
+    for (int j= i+1;j<n-1;j++)
+    for (int k=j+1;k<n;k++)
+    if (if arr[i]+arr[j]+arr[k]
+    return true;
+return false;
+)
+}
