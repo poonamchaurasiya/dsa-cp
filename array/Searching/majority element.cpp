4994bbfdc6e51662030474a59a7f3ecb37d0e81c
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-16 18:05:22 +0530

    using the repeating element logic

---
+++ array/Searching/majority element.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int find majority(int arr[],int)
+{
+    for (int i=0;i<n;i++)
+    {
+        int count =1,
+        for (int j =i+1;j<n;j++)
+        if(arr[i]==arr[j])
+        count ++;
+        if (count>n/2)
+
+}
+rteurn-1;
+}
