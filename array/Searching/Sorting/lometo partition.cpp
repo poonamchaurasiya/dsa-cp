0cf74b0224bfd5762ee553effc0d1d3fd24997cb
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-17 17:48:10 +0530

    using the hoare's partition logic

---
+++ array/Searching/Sorting/lometo partition.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int l partition(intarr[],int h)
+{
+    int pivot =arr[h];
+    int i =low-1;
+    for(int j =l;j=n-1;j++)
+    {
+        if(arr[j]<pivot)
+        {
+            i++;
+            swap(arr[i],arr[j]);
+            {
+                swap(arr[i+1],arr[h]);
+                return(i+1);
+            }
+        }
+    }
+}
