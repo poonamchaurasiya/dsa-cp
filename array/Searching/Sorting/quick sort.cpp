956154fc7a9e7c2bcc86507fbf86d8e76a9d13d6
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-17 17:58:12 +0530

    using the counting sort

---
+++ array/Searching/Sorting/quick sort.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+vodi quick sort(int arr[];int l,int r)
+{
+    if(r>l)
+    {
+        int p= partition (arr,l,r);
+        quick sort(arr,l,b)
+        quick sort(arr,p+1,r);
+    }
+}
