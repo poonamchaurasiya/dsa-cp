44882afaa38931026b4d99ac98f50219698ca7ad
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-18 16:35:29 +0530

    using the merge overlapping logic

---
+++ array/Searching/Sorting/an array with three types of ele.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+void sort(int arr[],int int n)
+{
+    for (int j=0;j<n;j++)
+    if(arr[j]==0){
+        temp[i]=arr[j];i++;
+
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]==1)
+            {
+                temp[i]=arr[j];i++;
+            }
+        }
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]==2)
+            {
+                temp[i]=arr[j];i++;
+            }
+        }
+    }
+}
