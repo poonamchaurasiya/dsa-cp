58c87412d264088747bd8f14bc43a9da8527c2f0
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-18 16:19:09 +0530

    using tail call elimination in quick sort

---
+++ array/Searching/Sorting/count inversion in array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int countinversions (int arr[],int n)
+{
+    int count=0;
+    for (int i=0;i<n-1;i++)
+    {
+        for (int j=i+1;j<n;j++)
+        {
+            if (arr[i]>arr[j])
+            count++;
+        }
+    }
+    return count;
+}
