3cc71be28caea756fc85ec74bd709788f3d8930c
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-20 20:44:14 +0530

    using the preorder logic

---
+++ array/Searching/Sorting/tree traversal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+void inorder(Node* root)
+{
+    if (root==NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
