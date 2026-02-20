0333f537c6a7131ce45bd10ecf5ff1481717c18d
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-02-20 20:42:26 +0530

    using the inorder logic in tree traversal

---
+++ array/Searching/Sorting/binary tree.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val)
+    {
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
