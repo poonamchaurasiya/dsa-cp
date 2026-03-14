bf180b1fe110235a7ad237873a76c476a24cef88
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-03-14 21:14:57 +0530

    using the contains duplictaes 3

---
+++ array/maximal squar.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        if(n == 0) return 0;
+        int m = matrix[0].size();
+        
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        int maxSide = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] == '1') {
+                    if(i == 0 || j == 0)
+                        dp[i][j] = 1;
+                    else
+                        dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                    
+                    maxSide = max(maxSide, dp[i][j]);
+                }
+            }
+        }
+
+        return maxSide * maxSide;
+    }
+};
