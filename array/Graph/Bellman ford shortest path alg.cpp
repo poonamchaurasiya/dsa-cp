5d4b208e9128513df18aa49f6a792f66c33d4df8
poonamchaurasiya <poonamchaurasiya@users.noreply.github.com>
2026-03-26 22:53:23 +0530

    sliding window logic

---
+++ array/Graph/Bellman ford shortest path alg.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+struct Edge {
+    int src, dest, weight;
+};
+
+void bellmanFord(int V, int E, int src, vector<Edge>& edges) {
+    
+    vector<int> dist(V, INT_MAX);
+    dist[src] = 0;
+
+    
+    for (int i = 1; i <= V - 1; i++) {
+        for (int j = 0; j < E; j++) {
+            int u = edges[j].src;
+            int v = edges[j].dest;
+            int weight = edges[j].weight;
+            if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+            }
+        }
+    }
+
+    
+    for (int j = 0; j < E; j++) {
+        int u = edges[j].src;
+        int v = edges[j].dest;
+        int weight = edges[j].weight;
+        if (dist[u] != INT_MAX && dist[u] + weight < dist[v]) {
+            cout << "Graph contains a negative weight cycle!" << endl;
+            return;
+        }
+    }
+
+    /
+    cout << "Vertex \t Distance from Source" << endl;
+    for (int i = 0; i < V; i++) {
+        cout << i << " \t\t " << (dist[i] == INT_MAX ? -1 : dist[i]) << endl;
+    }
+}
+
+int main() {
+    int V = 5; 
+    int E = 8; 
+    vector<Edge> edges = {
+        {0, 1, -1}, {0, 2, 4}, {1, 2, 3}, {1, 3, 2}, 
+        {1, 4, 2}, {3, 2, 5}, {3, 1, 1}, {4, 3, -3}
+    };
+
+    bellmanFord(V, E, 0, edges);
+
+    return 0;
+}
